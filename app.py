33dcc1debc9b45e6066cd4167252a0acb6985d95
Dibyanshu kar <dibyanshukar8@gmail.com>
2024-07-11 04:03:41 +0530

    some database changes

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:dibyanshu69@localhost/height_data'
 
-
-app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 class Data(db.Model):
     __tablename__ = 'data'
@@ -16,25 +15,24 @@ class Data(db.Model):
     def __init__(self,email_,height_):
         self.email_ = email_
         self.height_ = height_
-        
-
+    
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-@app.route('/success',methods=['POST'])
+@app.route('/success', methods=['POST'])
 def success():
     if request.method == 'POST':
-        email=request.form["email_name"]
-        height=request.form["height_name"]
-        print(email,height)
-        data=Data(email,height)
-        db.session.add(data)
-        db.session.commit()
+        email = request.form["email_name"]
+        height = request.form["height_name"]
+        print(email, height)
+        data = Data(email, height)
+        with app.app_context():
+            db.create_all()  # Create database tables if they don't exist
+            db.session.add(data)
+            db.session.commit()
         return render_template('success.html')
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
