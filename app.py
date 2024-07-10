fbf8678771fdcfe84eb3b8a92d9f04ceb142bf83
Dibyanshu kar <dibyanshukar8@gmail.com>
2024-07-10 17:28:11 +0530

    creating a class

--- app.py
+++ app.py
@@ -4,6 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/Height_collector' #Dictionary key-value pairs so that our app knows where to look for the database.
+db=SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__ = 'data'
+    id=db.Column(db.Integer, primary_key=True)
+    email_=db.Column(db.String(120),unique=True)
+    height_=db.Column(db.Integer)
+    
+    def __init__(self,email_,height_):
+        self.email_ = email_
+        self.height_ = height_
+        
+
 
 @app.route('/')
 def index():
