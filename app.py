d1991ec0ba4612039592f32c6250ecf2bb5f3e2d
Dibyanshu kar <dibyanshukar8@gmail.com>
2024-07-11 15:28:07 +0530

    changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from send_email import send_email
+from sqlalchemy.sql import functions
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:dibyanshu69@localhost/height_data'
@@ -36,6 +37,7 @@ def success():
                 db.create_all()  # Create database tables if they don't exist
                 db.session.add(data)
                 db.session.commit()
+                average_height=db.session.query()
             return render_template('success.html')
     return render_template('index.html',text="Seems like u have entered the email already !!")
 
