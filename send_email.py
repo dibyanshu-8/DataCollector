4d5750399d8c215df67aeeba202d1d40eba6b620
Dibyanshu kar <dibyanshukar8@gmail.com>
2024-07-11 13:14:37 +0530

    changes

--- send_email.py
+++ send_email.py
@@ -1,1 +1,11 @@
-def send_email
+import email
+def send_email(email,heigh):
+    from_email="dibyanshukarr@gmail.com"
+    from_password="job@12_45"
+    to_email=email
+    
+    
+    subject="Height data"
+    message="Hey there,your height is %s."% height
+    
+    
