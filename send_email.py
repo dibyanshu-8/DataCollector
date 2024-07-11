c3ef5635057cf1f2e26208b3e5c540690357c2fc
Dibyanshu kar <dibyanshukar8@gmail.com>
2024-07-11 13:16:31 +0530

    chneges

--- send_email.py
+++ send_email.py
@@ -1,4 +1,4 @@
-import email
+from email.mime.text import MIMEText
 def send_email(email,heigh):
     from_email="dibyanshukarr@gmail.com"
     from_password="job@12_45"
@@ -6,6 +6,6 @@ def send_email(email,heigh):
     
     
     subject="Height data"
-    message="Hey there,your height is %s."% height
+    message="Hey there,your height is %s."
     
     
