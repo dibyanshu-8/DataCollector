646669505488925be39d16d67f154758bc1f3781
Dibyanshu kar <dibyanshukar8@gmail.com>
2024-07-11 16:27:32 +0530

    changes

--- send_email.py
+++ send_email.py
@@ -1,13 +1,13 @@
 from email.mime.text import MIMEText
 import smtplib
-def send_email(email,height,average_height):
+def send_email(email,height,average_height,count):
     from_email="dibyanshu.karr@gmail.com"
     from_password="bwua ccea xxng hisu"
     to_email=email
     
     
     subject="Height data"
-    message="Hey there,your height is <strong> %s</strong>cm. Average height of all is %s." % (height,average_height)
+    message="Hey there,your height is <strong> %s</strong>cm. Average height of all is %s and that is calculated out %s of people" % (height,average_height,count)
     
     msg=MIMEText(message,'html')
     msg['subject']=subject
